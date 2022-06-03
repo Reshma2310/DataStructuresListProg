e1e1ee9cb9d7dcbbbb024a7606c2c2fb39305518
Reshma2310 <dreshma253@gmail.com>
2022-06-03 17:29:12 +0500

    [Reshma] - Checking if parantheses are balanced or not

---
+++ DataStructuresListProg/DataStructuresListProg/BalancedParanthesesClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresListProg
+{
+    internal class BalancedParanthesesClass
+    {
+        public void BalancedParentheses()
+        {
+            bool valid = false;
+            Console.WriteLine("Enter Arithmetic Expression:");
+            string expression = Console.ReadLine();
+            char[] charArray = expression.ToCharArray();
+            int size = charArray.Length;
+            for (int j = 0; j < size; j++)
+            {
+                if (charArray[j] == '(' || charArray[j] == '{' || charArray[j] == '[' || charArray[j] == ')' || charArray[j] == '}' || charArray[j] == ']')
+                {
+                    valid = true;
+                    break;
+                }
+            }
+            if (valid)
+            {
+                StackArray stack = new StackArray(size);
+                int i = 0;
+                while (i < size)
+                {
+                    if (charArray[i] == '(' || charArray[i] == '{' || charArray[i] == '[')
+                    {
+                        stack.Push(charArray[i]);
+                    }
+                    else if (charArray[i] == ')' || charArray[i] == '}' || charArray[i] == ']')
+                    {
+                        stack.Pop();
+                    }
+                    i++;
+                }
+                if (stack.IsEmpty())
+                {
+                    Console.WriteLine("Parentheses appeared in a Balanced fashion");
+                }
+                else
+                {
+                    Console.WriteLine("Parentheses are not appeared in a Balanced fashion");
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are no parentheses in given Input");
+            }
+        }
+    }
+}
---
+++ DataStructuresListProg/DataStructuresListProg/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DatastructurePrograms
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Checkin for Balanced Paranthesis");
+            DataStructuresListProg.BalancedParanthesesClass value = new DataStructuresListProg.BalancedParanthesesClass();
+            value.BalancedParentheses();
+        }
+    }
+}
